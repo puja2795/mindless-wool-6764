2e0eb3580ba999470f5b703f161b67a51680db4e
puja2795 <pujarinisahoo95@gmail.com>
2023-02-22 21:31:09 +0530

    home page

---
+++ industry-buying/src/Pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/Home.css";
+
+export const HomePage = () => {
+  const usenavigate = useNavigate();
+  const [displayusername, displayusernameupdate] = useState("");
+  useEffect(() => {
+    let username = sessionStorage.getItem("username");
+    if (username === "" || username === null) {
+      usenavigate("/login");
+    } else {
+      displayusernameupdate(username);
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-center">Welcome </h1>
+    </div>
+  );
+};
