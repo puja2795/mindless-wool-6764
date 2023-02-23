1802b92b9b65c2a607ebbc2be62573a4a61537ce
puja2795 <pujarinisahoo95@gmail.com>
2023-02-23 22:06:42 +0530

    day-3

--- industry-buying/src/App.js
+++ industry-buying/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { TempNavbar } from "./Components/TempNavbar";
 import MainRoutes from "./Pages/MainRoutes";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer theme="colored" position="top-right"></ToastContainer>
       <TempNavbar />
       <MainRoutes />
     </div>
