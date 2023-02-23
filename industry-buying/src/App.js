89014b4d31f0dd70d071f1363224e2bc62f6e73d
Abhishek Singh <sabhishek900@gmail.com>
2023-02-23 11:53:10 +0530

    tamp navbar added

--- industry-buying/src/App.js
+++ industry-buying/src/App.js
@@ -1,20 +1,13 @@
 import "./App.css";
-import { CartPage } from "./Components/CartFolder/CartPage";
+import { TempNavbar } from "./Components/TempNavbar";
+import MainRoutes from "./Pages/MainRoutes";
 
-// import { Products } from './Components/ProductDisplay/Products';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { HomePage } from "./Pages/HomePage";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
-import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
-      {/* <Products /> */}
-
-      {/* <ToastContainer theme="colored" position="top-center"></ToastContainer> */}
-      <CartPage />
+      <TempNavbar />
+      <MainRoutes />
     </div>
   );
 }
--- industry-buying/src/Pages/HomePage.jsx
+++ industry-buying/src/Pages/HomePage.jsx
@@ -4,16 +4,16 @@ import { useNavigate } from "react-router-dom";
 import "../styles/Home.css";
 
 export const HomePage = () => {
-  const usenavigate = useNavigate();
-  const [displayusername, displayusernameupdate] = useState("");
-  useEffect(() => {
-    let username = sessionStorage.getItem("username");
-    if (username === "" || username === null) {
-      usenavigate("/login");
-    } else {
-      displayusernameupdate(username);
-    }
-  }, []);
+  // const usenavigate = useNavigate();
+  // const [displayusername, displayusernameupdate] = useState("");
+  // useEffect(() => {
+  //   let username = sessionStorage.getItem("username");
+  //   if (username === "" || username === null) {
+  //     usenavigate("/login");
+  //   } else {
+  //     displayusernameupdate(username);
+  //   }
+  // }, []);
 
   return (
     <div>
