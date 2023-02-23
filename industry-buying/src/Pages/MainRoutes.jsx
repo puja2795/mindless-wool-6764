6790dc52abb732f3ffa0d0e0361ff9a221a9f854
TOFANDONNO1 <ranjansoumya413@gmail.com>
2023-02-23 21:06:20 +0530

    added

--- industry-buying/src/Pages/MainRoutes.jsx
+++ industry-buying/src/Pages/MainRoutes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { CartPage } from "../Components/CartFolder/CartPage";
+import PaymentPage from "../Components/CartFolder/PaymentPage";
 import { Products } from "../Components/ProductDisplay/Products";
 import { HomePage } from "./HomePage";
 import Login from "./Login";
@@ -14,6 +15,7 @@ export default function MainRoutes() {
       <Route path="/register" element={<Register />}></Route>
       <Route path="/product" element={<Products />} />
       <Route path="/cart" element={<CartPage />} />
+      <Route path="/paymentpage" element={<PaymentPage/>}/>
     </Routes>
   );
 }
