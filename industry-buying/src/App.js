46b1f0423a8afd49f3037a791067275ff6db4fce
kapil <kapil.maywade2014@gmail.com>
2023-02-23 23:34:04 +0530

    navbar

--- industry-buying/src/App.js
+++ industry-buying/src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import { TempNavbar } from "./Components/TempNavbar";
 import MainRoutes from "./Pages/MainRoutes";
-
+// import Navbar from "./Pages/Navbar";
+// import Sidebar from "./Pages/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <TempNavbar />
       <MainRoutes />
+      {/* <Navbar/>
+      <Sidebar/> */}
     </div>
   );
 }
--- industry-buying/src/Pages/Navbar.jsx
+++ industry-buying/src/Pages/Navbar.jsx
@@ -1,7 +1,40 @@
-import React from "react";
-const Navbar = () =>{
-    <div>
+import React, { useState } from 'react';
 
-    </div>
+import "./Navbar.css"
+const Navbar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    console.log(`Searching for "${searchTerm}"...`);
+    // implement search functionality here
+  };
+
+  return (
+    <nav>
+      <div className="nav-container">
+        <div className="logo-container">
+          {/* <img src={} alt="Logo" /> */}
+          <p>indestrialbying</p>
+        </div>
+        <div className="search-container">
+          <input type="text" placeholder="Search Products" 
+             value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button onClick={handleSearch}>Search logo</button>
+        </div>
+        <div className="menu-container">
+          <ul>
+          <li><a href="#">Sign In</a></li>
+            <li><a href="#">Track Order</a></li>
+            <li><a href="#">cart</a></li>
+            <li><a href="https://play.google.com/store">Play Store logo</a></li>        <li><a href="https://play.google.com/store"> apple Store logo</a></li>            
+          </ul>
+        </div>
+        
+      </div>
+    </nav>
+  );
 }
-export default  Navbar;
+
+export default Navbar;
