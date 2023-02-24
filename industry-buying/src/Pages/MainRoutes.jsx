0aac02ac400cb707c329d538f1a3eefc0eba1462
rinki <rinki9414@gmail.com>
2023-02-24 21:59:17 +0530

    day-4

--- industry-buying/src/Pages/MainRoutes.jsx
+++ industry-buying/src/Pages/MainRoutes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { CartPage } from "../Components/CartFolder/CartPage";
+import { Admin } from "./Admin";
 
 import { HomePage } from "./HomePage";
 import Login from "./Login";
@@ -15,6 +16,8 @@ export default function MainRoutes() {
       <Route path="/register" element={<Register />}></Route>
       <Route path="/product" element={<Products />} />
       <Route path="/cart" element={<CartPage />} />
+      <Route path="/admin" element={<Admin />} />
+
     </Routes>
   );
 }
--- industry-buying/src/index.js
+++ industry-buying/src/index.js
@@ -6,13 +6,22 @@ import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </BrowserRouter>
+
+    <BrowserRouter>
+
+
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+
+
+
+    </BrowserRouter>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
