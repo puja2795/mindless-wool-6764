a9499ea54a217584bfeb59a3f53ed26be539c225
puja2795 <pujarinisahoo95@gmail.com>
2023-02-24 22:05:40 +0530

    day-4

---
+++ industry-buying/src/Components/HomePageComponents/Furniture.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import data from "../../data.json";
+import Slider from "./Slider";
+import { Grid, Heading } from "@chakra-ui/react";
+
+const Furniture = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    setProducts(data.furniture);
+  }, []);
+
+  return (
+    <div>
+      <Heading as="h3" size="md" textAlign="left" marginTop="12px">
+        FURNITURE, HOSPITALITY AND FOOD SERVICE
+      </Heading>
+      <Slider items={products} columns={8} type="card" />
+    </div>
+  );
+};
+
+export default Furniture;
--- industry-buying/src/Pages/MainRoutes.jsx
+++ industry-buying/src/Pages/MainRoutes.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { CartPage } from "../Components/CartFolder/CartPage";
 
 import PaymentPage from "../Components/CartFolder/PaymentPage";
-import { Products } from "../Components/ProductDisplay/Products";
+// import { Products } from "../Components/ProductDisplay/Products";
 
 import { HomePage } from "./HomePage";
 import Login from "./Login";
@@ -18,7 +18,7 @@ export default function MainRoutes() {
       <Route path="/register" element={<Register />}></Route>
       <Route path="/product" element={<Products />} />
       <Route path="/cart" element={<CartPage />} />
-      <Route path="/paymentpage" element={<PaymentPage/>}/>
+      <Route path="/paymentpage" element={<PaymentPage />} />
     </Routes>
   );
 }
