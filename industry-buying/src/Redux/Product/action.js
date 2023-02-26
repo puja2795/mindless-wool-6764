7d91ca3014060809474e35e92963c48497a9561c
Abhishek Singh <sabhishek900@gmail.com>
2023-02-26 13:07:29 +0530

    product page updated

--- industry-buying/src/Redux/Product/action.js
+++ industry-buying/src/Redux/Product/action.js
@@ -1,4 +1,5 @@
-import { GET_PPRODUCTS_FAILURE, GET_PPRODUCTS_REQUEST, GET_PPRODUCTS_SUCCESS } from "./actionType"
+import axios from "axios";
+import { GET_PPRODUCTS_FAILURE, GET_PPRODUCTS_REQUEST, GET_PPRODUCTS_SUCCESS, POST_PPRODUCTS_CART_FAILURE, POST_PPRODUCTS_CART_REQUEST, POST_PPRODUCTS_CART_SUCCESS } from "./actionType"
 
 
 
@@ -14,3 +15,56 @@ export const productSuccessAction = (payload) => {
 export const productFailureAction = () => {
     return { type: GET_PPRODUCTS_FAILURE };
 }
+
+
+export const getData = () => (dispatch) => {
+    dispatch(productRequestAction());
+
+    axios
+      .get(`http://localhost:8080/powertools`)
+      .then((res) => {
+        // console.log(res.data);
+        dispatch(productSuccessAction(res.data));
+      })
+      .catch((err) => {
+        // console.log(err);
+        dispatch(productFailureAction());
+      });
+    };
+
+
+    // Cart Post
+
+    export const postCartRequestAction = () => {
+
+        return { type: POST_PPRODUCTS_CART_REQUEST };
+    }
+
+    export const postCartSuccessAction = (payload) => {
+
+        return { type: POST_PPRODUCTS_CART_SUCCESS, payload: payload };
+    }
+
+    export const postCartFailureAction = () => {
+
+        return { type: POST_PPRODUCTS_CART_FAILURE };
+    }
+
+
+    export const handleCart = (id,payload) => (dispatch) => {
+
+       // console.log("Product");
+       //console.log(payload);
+
+        dispatch(postCartRequestAction());
+    
+        axios.post(`http://localhost:8080/cart`,payload)
+        .then((res) => {
+            console.log(res.data);
+            dispatch(postCartSuccessAction(res.data));
+        })
+        .catch((err) => {
+            dispatch(postCartFailureAction())
+          console.log(err);
+        })
+      }
