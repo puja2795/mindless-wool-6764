be3e62715ed2886fa38ece640620e7be37142350
Abhishek Singh <sabhishek900@gmail.com>
2023-02-27 11:16:01 +0530

    changes made in footer

--- industry-buying/src/Footer.jsx
+++ industry-buying/src/Footer.jsx
@@ -11,7 +11,7 @@ import {
     IconButton,
     useColorModeValue,
   } from '@chakra-ui/react';
-  import { ReactNode } from 'react';
+  // import { ReactNode } from 'react';
   import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
   import { BiMailSend } from 'react-icons/bi';
   
@@ -74,7 +74,7 @@ import {
       <Box
       className='footer'
         bg={useColorModeValue('gray.50', 'gray.900')}
-        color={useColorModeValue('gray.700', 'gray.200')}>
+        color={useColorModeValue('gray.700', 'gray.200')} marginTop={"80px"}>
         <Container as={Stack} maxW={'6xl'} py={10}>
           <SimpleGrid
             templateColumns={{ sm: '1fr 1fr', md: '2fr 1fr 1fr 2fr' }}
